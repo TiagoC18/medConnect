b1b9efcfd50665e4fb01865618f3b035e269fb01
Gonçalo Lopes <goncalorcml@ua.pt>
2024-05-13 22:43:25 +0100

    backend start

---
+++ medConnect/src/test/java/project/medConnect/MedConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.medConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
